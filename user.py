0d54483bf525909cacf4fe57a511156b340efb9e
Kevin Kleinfeldt <kevinkleinfeldt@icloud.com>
2024-03-18 14:37:49 +0100

    implemented function to ask user for languages spoken

--- user.py
+++ user.py
@@ -5,4 +5,15 @@ class User():
        self.known_podcasts = []
        self.languages = []
     
-    
+    def get_languages(self):
+         new_language = True
+         while True:
+             language_spoken = input("Which language do you speak? Please name one. ")
+             language_to_append = language_spoken.lower()
+             self.languages.append(language_to_append)
+             another_one = input("Do you speak another language? y/n ")
+             if another_one =="y":
+                 continue
+             else:
+                 new_language = False
+         return self.languages
